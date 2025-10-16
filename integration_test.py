f31c17dc2e6158372edb394c9b4809be89aa2052
Christopher Scott <cjscott05@icloud.com>
2025-10-16 15:55:50 +0100

    Fix indentation issues in integration tests

--- integration_test.py
+++ integration_test.py
@@ -1,29 +1,20 @@
 import unittest
-
 from app import app
 
 class TestCalculationsIntegration(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
 
-        app.testing = True
+    def test_calculation_integration(self):
+        response = self.client.post("/", data={"a": "8", "b": "2"})
 
-        self.client = app.test_client()
-
-    def test_calculation_integration(self):
-
-        response = self.client.post("/", data={"a": "8", "b": "2"})
-
-        html = response.data.decode("utf-8")
-
-        self.assertIn("Sum: 10.0", html)
-
-        self.assertIn("Difference: 6.0", html)
-
-        self.assertIn("Product: 16.0", html)
-
-        self.assertIn("Quotient: 4.0", html)
+        html = response.data.decode("utf-8")
+        self.assertIn("Sum: 10.0", html)
+        self.assertIn("Difference: 6.0", html)
+        self.assertIn("Product: 16.0", html)
+        self.assertIn("Quotient: 4.0", html)
 
 if __name__ == "__main__":
-
-    unittest.main()
+    unittest.main()
